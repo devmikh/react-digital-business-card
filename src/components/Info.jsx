3e9947ab526a12d50323ad8c6393f8ebc552b7e3
Mikhail Serebriakov <mishacyb@gmail.com>
2022-06-17 18:55:42 -0700

    Changed CSS for resume link

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -4,15 +4,18 @@ import { BsLinkedin } from 'react-icons/bs';
 
 export default function Info() {
     return (
-        <div className="info">
-            <img src={photo} alt="Mikhail" className="info--photo" />
-            <h1 className="info--name">Mikhail Serebriakov</h1>
-            <h3 className="info--occupation">Front-End Developer</h3>
-            <a href="https://resume.creddle.io/resume/9bs4b9bxn9r" target="_blank" rel="noreferrer" className="info--website">My Resume</a>
-            <div className="info--buttons">
-                <a href="mailto:mishacyb@gmail.com" className="info--buttons--email"><GrMail /><span className="info--buttons--text">Email</span></a>
-                <a href="https://www.linkedin.com/in/serebriakovm/" target="_blank" rel="noreferrer" className="info--buttons--linkedin"><BsLinkedin /><span className="info--buttons--text">LinkedIn</span></a>
+            <div className="info">
+                <img src={photo} alt="Mikhail" className="info--photo" />
+                <h1 className="info--name">Mikhail Serebriakov</h1>
+                <h3 className="info--occupation">Front-End Developer</h3>
+                <div className="info--website">
+                    <a href="https://resume.creddle.io/resume/9bs4b9bxn9r" target="_blank" rel="noreferrer">My Resume</a>
+                </div>
+                <div className="info--buttons">
+                    <a href="mailto:mishacyb@gmail.com" className="info--buttons--email"><GrMail /><span className="info--buttons--text">Email</span></a>
+                    <a href="https://www.linkedin.com/in/serebriakovm/" target="_blank" rel="noreferrer" className="info--buttons--linkedin"><BsLinkedin /><span className="info--buttons--text">LinkedIn</span></a>
+                </div>
             </div>
-        </div>
+        
     )
 }
