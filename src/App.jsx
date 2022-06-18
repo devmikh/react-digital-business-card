43f91b1f9fbb6afca22dae15193fcd5d5fa7f030
Mikhail Serebriakov <mishacyb@gmail.com>
2022-06-17 18:10:07 -0700

    Added light/dark theme switch

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,38 @@ import Info from './components/Info';
 import About from './components/About';
 import Interests from './components/Interests';
 import Footer from './components/Footer';
+import ReactSwitch from 'react-switch';
+
+import { createContext, useState } from "react";
+
+export const ThemeContext = createContext(null);
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((currentTheme) => (currentTheme === "light" ? "dark" : "light"));
+  }
   return (
-    <div className="container">
-      <Info />
-      <About />
-      <Interests />
-      <Footer />
-    </div>
+    <ThemeContext.Provider value={{theme, toggleTheme}}>
+      
+        <div className="App" id={theme}>
+          <div className="container">
+            <div className="theme-switch">
+              <label>{theme === "light" ? "Light Theme" : "Dark Theme"}</label>
+              <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} />
+            </div>
+            <div className="card-container">
+              <Info />
+              <About />
+              <Interests />
+              <Footer />
+            </div>
+          </div>
+        </div>
+      
+      
+    </ThemeContext.Provider>
   );
 }
 
